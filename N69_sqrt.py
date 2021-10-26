9be611a545353f0dd36a6319075347dae088e5e6
Alex <beatwad@gmail.com>
2021-10-26 23:33:21 +0300

    Minor fixes in N69.

--- N69_sqrt.py
+++ N69_sqrt.py
@@ -1,24 +1,30 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        a = x // 2
-        prev_a = a
-        while True:
-            if a**2 == x or a**2 < x <= (a+1)**2:
-                return a
-            if a**2 < x:
-                a = a + (prev_a - a) // 2
+        ans, low, high = 0, 0, x
+        while low <= high:
+            mid = low + (high - low) // 2
+            if mid * mid == x:
+                return mid
+            if mid * mid < x:
+                ans = mid
+                low = mid + 1
             else:
-                prev_a = a
-                a = a // 2
+                high = mid - 1
+        return ans
 
 
 if __name__ == '__main__':
     sol = Solution()
-    # assert sol.mySqrt(0) == 0
+    assert sol.mySqrt(0) == 0
     assert sol.mySqrt(1) == 1
-    # assert sol.mySqrt(8) == 2
-    # assert sol.mySqrt(4) == 2
-    # assert sol.mySqrt(25) == 5
+    assert sol.mySqrt(2) == 1
+    assert sol.mySqrt(8) == 2
+    assert sol.mySqrt(4) == 2
+    assert sol.mySqrt(25) == 5
+    assert sol.mySqrt(169) == 13
+    assert sol.mySqrt(1369) == 37
+    assert sol.mySqrt(1370) == 37
+    assert sol.mySqrt(2**31-1) == 46340
 
 
 
