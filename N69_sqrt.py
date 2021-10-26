85de70db474ee083abe2fd03575ba51c0fbe8802
Alex <beatwad@gmail.com>
2021-10-26 18:26:14 +0300

    Temp commit

--- N69_sqrt.py
+++ N69_sqrt.py
@@ -1,18 +1,24 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         a = x // 2
+        prev_a = a
         while True:
-            if a**2 == x or a**2 < x < (a+1)**2:
+            if a**2 == x or a**2 < x <= (a+1)**2:
                 return a
             if a**2 < x:
-                a = 3*a // 2
+                a = a + (prev_a - a) // 2
             else:
+                prev_a = a
                 a = a // 2
 
 
 if __name__ == '__main__':
     sol = Solution()
-    assert sol.mySqrt(25) == 5
+    # assert sol.mySqrt(0) == 0
+    assert sol.mySqrt(1) == 1
+    # assert sol.mySqrt(8) == 2
+    # assert sol.mySqrt(4) == 2
+    # assert sol.mySqrt(25) == 5
 
 
 
